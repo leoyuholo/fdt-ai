6c0cf0c64a866993112c23729cf3b0c0fd91c7cc
leoyuholo <leoyuholo@gmail.com>
2017-06-21 00:01:41 +0800

    add testcases/tiebreak

--- test/ProfitTable.test.js
+++ test/ProfitTable.test.js
@@ -33,6 +33,28 @@ describe('lib', () => {
 				ranking[1].traderId.should.be.equal('BBB');
 				ranking[1].profit.valueOf().should.be.equal('-252.96');
 			});
+
+			it('should tie break by trader id', () => {
+				const table = new ProfitTable();
+				table.addOrder({ OrderId: '1', Trader: 'AAA', StkCode: '222222', Quantity: '100', Price: '10', TradeType: 'Buy', Fee: '0.2', Timestamp: '2016-05-09 13:38:24' });
+				table.addOrder({ OrderId: '2', Trader: 'CCC', StkCode: '222222', Quantity: '100', Price: '10', TradeType: 'Buy', Fee: '0.2', Timestamp: '2016-05-09 13:38:25' });
+				table.addOrder({ OrderId: '3', Trader: 'BBB', StkCode: '333333', Quantity: '1000', Price: '8.8', TradeType: 'Sell', Fee: '10.56', Timestamp: '2016-05-09 10:37:16' });
+				table.addOrder({ OrderId: '4', Trader: 'AAA', StkCode: '222222', Quantity: '100', Price: '11', TradeType: 'Sell', Fee: '1.32', Timestamp: '2016-05-10 13:38:24' });
+				table.addOrder({ OrderId: '5', Trader: 'CCC', StkCode: '222222', Quantity: '100', Price: '11', TradeType: 'Sell', Fee: '1.32', Timestamp: '2016-05-10 13:38:25' });
+				table.addOrder({ OrderId: '6', Trader: 'BBB', StkCode: '333333', Quantity: '800', Price: '9.0', TradeType: 'Buy', Fee: '1.6', Timestamp: '2016-05-11 14:58:30' });
+				table.addOrder({ OrderId: '7', Trader: 'BBB', StkCode: '333333', Quantity: '400', Price: '9.2', TradeType: 'Buy', Fee: '0.79', Timestamp: '2016-05-16 10:28:24' });
+				table.addOrder({ OrderId: '8', Trader: 'BBB', StkCode: '333333', Quantity: '500', Price: '9.2', TradeType: 'Buy', Fee: '0.89', Timestamp: '2016-05-16 10:28:24' });
+				table.addOrder({ OrderId: '9', Trader: 'AAA', StkCode: '555555', Quantity: '600', Price: '15.5', TradeType: 'Buy', Fee: '2.0', Timestamp: '2016-05-16 10:31:33' });
+				table.addOrder({ OrderId: '10', Trader: 'CCC', StkCode: '555555', Quantity: '600', Price: '15.5', TradeType: 'Buy', Fee: '2.0', Timestamp: '2016-05-16 10:31:34' });
+				const ranking = table.computeRanking();
+				ranking.length.should.be.equal(3);
+				ranking[0].traderId.should.be.equal('AAA');
+				ranking[0].profit.valueOf().should.be.equal('96.48');
+				ranking[1].traderId.should.be.equal('CCC');
+				ranking[1].profit.valueOf().should.be.equal('96.48');
+				ranking[2].traderId.should.be.equal('BBB');
+				ranking[2].profit.valueOf().should.be.equal('-253.84');
+			});
 		});
 	});
 });

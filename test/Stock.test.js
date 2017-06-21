f28e28a0bb1a39e61416739dbbc081fe077bd056
leoyuholo <leoyuholo@gmail.com>
2017-06-21 23:15:10 +0800

    fix lint

--- test/Stock.test.js
+++ test/Stock.test.js
@@ -9,38 +9,38 @@ describe('lib', () => {
 		describe('computeProfit', () => {
 			it('should compute profit for 1 buy and 1 sell', () => {
 				const stock = new Stock();
-				stock.buy(1, 0.7, 0, new Date());
-				stock.sell(1, 0.8, 0, new Date());
+				stock.buy(1, 0.7, 0);
+				stock.sell(1, 0.8, 0);
 				stock.computeProfit().valueOf().should.be.equal('0.1');
 			});
 
 			it('should compute profit for short selling', () => {
 				const stock = new Stock();
-				stock.sell(8, 1.2, 0, new Date());
-				stock.buy(8, 1, 0, new Date());
+				stock.sell(8, 1.2, 0);
+				stock.buy(8, 1, 0);
 				stock.computeProfit().valueOf().should.be.equal('1.6');
 			});
 
 			it('should compute only realized profit', () => {
 				const stock = new Stock();
-				stock.sell(8, 1.2, 0, new Date());
-				stock.buy(8, 1, 0, new Date());
-				stock.buy(8, 1, 0, new Date());
+				stock.sell(8, 1.2, 0);
+				stock.buy(8, 1, 0);
+				stock.buy(8, 1, 0);
 				stock.computeProfit().valueOf().should.be.equal('1.6');
 			});
 
 			it('should be paying transaction fee', () => {
 				const stock = new Stock();
-				stock.buy(1, 0.7, 0.035, new Date());
-				stock.sell(1, 0.8, 0.04, new Date());
+				stock.buy(1, 0.7, 0.035);
+				stock.sell(1, 0.8, 0.04);
 				stock.computeProfit().valueOf().should.be.equal('0.025');
 			});
 
 			it('should be paying transaction fee even not yet realized', () => {
 				const stock = new Stock();
-				stock.sell(8, 1.2, 0.04, new Date());
-				stock.buy(8, 1, 0.03, new Date());
-				stock.buy(8, 1, 0.03, new Date());
+				stock.sell(8, 1.2, 0.04);
+				stock.buy(8, 1, 0.03);
+				stock.buy(8, 1, 0.03);
 				stock.computeProfit().valueOf().should.be.equal('1.5');
 			});
 		});
--- test/Trader.test.js
+++ test/Trader.test.js
@@ -9,20 +9,20 @@ describe('lib', () => {
 		describe('computeProfit', () => {
 			it('should compute profit for one stock', () => {
 				const trader = new Trader();
-				trader.buy('GOOGL', 352, 975.22, 343.27744, new Date());
-				trader.buy('GOOGL', 253, 1075.22, 272.03066, new Date());
-				trader.sell('GOOGL', 605, 1100.22, 665.6331, new Date());
+				trader.buy('GOOGL', 352, 975.22, 343.27744);
+				trader.buy('GOOGL', 253, 1075.22, 272.03066);
+				trader.sell('GOOGL', 605, 1100.22, 665.6331);
 				trader.computeProfit().valueOf().should.be.equal('49044.0588');
 			});
 
 			it('should compute profit for more than one stock', () => {
 				const trader = new Trader();
-				trader.buy('GOOGL', 352, 975.22, 343.27744, new Date());
-				trader.buy('GOOGL', 253, 1075.22, 272.03066, new Date());
-				trader.sell('GOOGL', 605, 1100.22, 665.6331, new Date());
-				trader.sell('AAPL', 4340, 146.34, 635.1156, new Date());
-				trader.buy('AAPL', 7560, 166.34, 1257.5304, new Date());
-				trader.sell('AAPL', 2240, 253.45, 567.728, new Date());
+				trader.buy('GOOGL', 352, 975.22, 343.27744);
+				trader.buy('GOOGL', 253, 1075.22, 272.03066);
+				trader.sell('GOOGL', 605, 1100.22, 665.6331);
+				trader.sell('AAPL', 4340, 146.34, 635.1156);
+				trader.buy('AAPL', 7560, 166.34, 1257.5304);
+				trader.sell('AAPL', 2240, 253.45, 567.728);
 				trader.computeProfit().valueOf().should.be.equal('154910.0848');
 			});
 		});
